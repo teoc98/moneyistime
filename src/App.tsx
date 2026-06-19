621fe563034dcc201a406edcd3927ac6d7ac02c5
Matteo Cavallaro <teoc98@users.noreply.github.com>
2026-06-19 15:18:08 +0200

    pico and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import type { Component } from 'solid-js';
-import Comp from './Comp';
+import type { Component } from "solid-js";
+import Comp from "./Comp";
 
 const App: Component = () => {
   return (
