536ab49d3429462341ac762d6f17a2131af78e4c
Matteo Cavallaro <teoc98@users.noreply.github.com>
2026-06-20 01:32:54 +0200

    improve Table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,22 +1,41 @@
 import type { Component } from "solid-js";
 import { For } from "solid-js";
 
-interface TableProps {
-  data: Array<Array<string | (() => number)>>;
+interface ColumnFormat<T> {
+  format?: (value: T) => string;
+}
+
+interface TableProps<T> {
+  data: Array<Array<T>>;
+  columnFormat?: Array<ColumnFormat | undefined>;
   class?: string;
 }
 
+function extendArray(arr: Array<any>, n: number, value: any = undefined) {
+  return arr.length >= n
+    ? arr.slice(0, n)
+    : arr.concat(Array(n - arr.length).fill(value));
+}
+
 const Table: Component<TableProps> = (props) => {
-  const renderValue = (value: string | (() => number)) => {
-    return typeof value === "function" ? value() : value;
+  const renderValue = (value: string | (() => number), colIndex: number) => {
+    const raw = typeof value === "function" ? value() : value;
+    const config = props.columnFormat?.[colIndex];
+    if (config?.format && typeof raw === "number") {
+      return config.format(raw);
+    }
+    return raw;
   };
+  const nCols = Math.max(...props.data.map((row) => row.length));
 
   return (
     <table class={`${props.class ?? ""}`}>
       <thead>
         <tr>
           <For each={props.data[0]}>
-            {(cell) => <th scope="col">{renderValue(cell)}</th>}
+            {(cell, cellIndex) => (
+              <th scope="col">{renderValue(cell, cellIndex())}</th>
+            )}
           </For>
         </tr>
       </thead>
@@ -24,12 +43,12 @@ const Table: Component<TableProps> = (props) => {
         <For each={props.data.slice(1)}>
           {(row, index) => (
             <tr>
-              <For each={row}>
+              <For each={extendArray(row, nCols)}>
                 {(cell, cellIndex) =>
                   cellIndex() === 0 ? (
-                    <th scope="row">{renderValue(cell)}</th>
+                    <th scope="row">{renderValue(cell, cellIndex())}</th>
                   ) : (
-                    <td>{renderValue(cell)}</td>
+                    <td>{renderValue(cell, cellIndex())}</td>
                   )
                 }
               </For>
