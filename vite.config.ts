bde935f546e5aed7151793770f293bfe9534a2a7
Matteo Cavallaro <teoc98@users.noreply.github.com>
2026-06-20 17:34:58 +0200

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import solidPlugin from "vite-plugin-solid";
 import devtools from "solid-devtools/vite";
 
-export default defineConfig({
-  plugins: [devtools(), solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: "esnext",
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: env.VITE_BASE_URL || "/",
+    plugins: [devtools(), solidPlugin()],
+    server: {
+      port: 3000,
+    },
+    build: {
+      target: "esnext",
+    },
+  };
 });
